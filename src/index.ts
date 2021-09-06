f13b135a4136caefb7b733e69cb1e50ec4604094
David Ilie <david@davidilie.com>
2021-09-06 10:29:47 +0200

    fixes everywhere

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,41 @@ const main = async () => {
 
     app.set("view engine", "ejs");
 
-    app.get("/video", (_req: express.Request, res: express.Response) => {
-        request(`${process.env.OCTO_PRINT_URL}/webcam/?action=stream`).pipe(
-            res
-        );
+    app.get("/video", async (_req: express.Request, res: express.Response) => {
+        const jobR = await octoRequest(`/api/job`);
+        const jobResponse = await jobR.data;
+
+        const printerR = await octoRequest(`/api/printer`);
+        const printerResponse = await printerR.data;
+
+        const printingState =
+            printerResponse.state.text === "Operational"
+                ? "Nothing"
+                : jobResponse.state === "Printing"
+                ? jobResponse.progress.completion <= 0.1
+                    ? "Heating"
+                    : "Printing"
+                : "Nothing";
+
+        if (printingState === "Nothing") {
+            res.render("../public/index", {
+                printingState,
+                temps: {
+                    bed: printerResponse.temperature.bed.actual,
+                    nozzle: printerResponse.temperature.tool0.actual,
+                },
+                file: {
+                    name: jobResponse.job.file.name,
+                    duration: durationFormatter(
+                        jobResponse.progress.printTimeLeft
+                    ),
+                },
+            });
+        } else {
+            request(`${process.env.OCTO_PRINT_URL}/webcam/?action=stream`).pipe(
+                res
+            );
+        }
     });
 
     app.get("/", async (_req: express.Request, res: express.Response) => {
